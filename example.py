282379ca57d701f007a40bac7069bfdef54da726
Harnit Singh <harnit.singh@signalfx.com>
2019-08-26 14:11:44 -0700

    propagating traces

--- example.py
+++ example.py
@@ -4,6 +4,8 @@ from jaeger_client import Config
 import opentracing
 from opentracing.ext import tags
 import signalfx_lambda
+from signalfx_tracing import auto_instrument
+from signalfx_tracing.libraries import requests_config 
 
 
 class InjectTrace(object):
@@ -15,8 +17,14 @@ class InjectTrace(object):
 		span_tags = {tags.SPAN_KIND: tags.SPAN_KIND_RPC_SERVER}
 		with self.tracer.start_span('handle_request', tags=span_tags) as span:
 			span.set_tag('aws_request_id',context.aws_request_id)
-			response = requests.post("http://35.236.100.236:6000/echo")
-			print ('response')
+			requests_config.propagate = True
+			requests_config.tracer = self.tracer
+
+			auto_instrument()  # or instrument(requests=True)
+			traced_session = requests.Session()
+			traced_post_response = traced_session.post("http://35.236.100.236:6000/echo")
+			#response = requests.post("http://35.236.100.236:6000/echo")
+			print ('response->',traced_post_response)
 		return response
 
 # Our registered Lambda handler entrypoint (example.request_handler) with the
